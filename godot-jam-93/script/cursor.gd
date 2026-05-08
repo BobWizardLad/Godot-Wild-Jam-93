e3cab19b4f474605b68ab924baf74774fa4775ec
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-08 18:21:04 -0500

    Twin shooter test updated with cursor behavior
    Cursor sprite implemented (camera 2.5x zoom right now, cursor is smol)
    Cursor will hide the mouse when it is processing (not paused,
    is_processing(), etc)
    and show the mouse otherwise.

--- godot-jam-93/script/cursor.gd
+++ godot-jam-93/script/cursor.gd
@@ -12,6 +12,11 @@ extends Node2D
 # Follows the mouse's position every frame
 func _process(delta: float) -> void:
 	global_position = lerp(global_position, get_global_mouse_position(), lerp_strength)
+	
+	if is_processing():
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 ## Returns the global_position of this cursor
 func get_target() -> Vector2:
