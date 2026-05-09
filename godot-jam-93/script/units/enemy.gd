750696630d694b134f489f0fd21d10832d89abec
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-09 15:06:19 -0500

    Charger movement effective
    Charger movemetn is fairly predictable, also charge logic was done, just
    buggy
    
    Charger is cleaned up, and attacks connect from the charger (still had
    collision layer rule breaks)

--- godot-jam-93/script/units/enemy.gd
+++ godot-jam-93/script/units/enemy.gd
@@ -23,7 +23,7 @@ func _physics_process(_delta: float) -> void:
 ## Function that returns the calculated velocity of a unit.
 func derive_unit_velocity() -> Vector2:
 	#nav_agent.target_position = get_tree().get_first_node_in_group("Player").global_position
-	if !nav_agent.is_target_reached() && nav_agent.distance_to_target() < 300:
+	if !nav_agent.is_target_reached():
 		var nav_point_direction = to_local(nav_agent.get_next_path_position()).normalized()
 		return nav_point_direction * SPEED
 	else:
