783d68a11095b7fe0ba4b4d45764df2d2b64ecb8
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-09 09:01:39 -0500

    Total Collisions Rewrite
    Each discrete object type has its own physic collision layer and each
    unit team has a damage recieved layer
    
    Player collides on 1 and layers damage on 5
    Enemies collide on 3 and layer damage on 6
    bullets collide on layer 2
    
    e.g. anything meant to damage the player masks layer 5, damage to the
    enemies masks layer 6

--- godot-jam-93/script/bullet.gd
+++ godot-jam-93/script/bullet.gd
@@ -15,18 +15,20 @@ func _ready():
 
 func _physics_process(delta: float):
 	move_and_slide()
+	if get_slide_collision_count() > 0:
+		collide_and_expire(self)
 
 func expire_timer_start(time: float):
 	lifetimer.timeout.connect(expire)
 	lifetimer.start(lifetime)
 
 ## (.(
-func collide_and_expire(body: Node2D):
-	assert($DamageArea != null, "No DamageArea named DamageArea!")
-	$DamageArea.queue_free()
-	velocity = velocity * Vector2(0.25, 0.25)
-	lifetimer.stop()
-	animation_player.play("expire")
+func collide_and_expire(_body: Node2D):
+	if $DamageArea != null:
+		$DamageArea.queue_free()
+		velocity = velocity * Vector2(0.25, 0.25)
+		lifetimer.stop()
+		animation_player.play("expire")
 	
 func expire():
 	queue_free()
