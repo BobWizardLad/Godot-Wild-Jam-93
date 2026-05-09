51b13aaaa789e550984f5d993bc99dc6ca3de65f
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-08 20:03:35 -0500

    Attached a Nav Agent to enemy
    Enemy is forming with a controller to handle advanced AI decisions away
    from unit object subclass.
    NavAgent is implemented to handle routing to the player
    SoftAnimationPlayer is in enemy to animate damage

--- godot-jam-93/script/enemy.gd
+++ godot-jam-93/script/enemy.gd
@@ -1,2 +1,15 @@
 class_name Enemy
 extends Unit
+
+@onready var soft_animation_player: AnimationPlayer = $SoftAnimationPlayer
+@onready var nav_agent: NavigationAgent2D = $NavAgent
+
+# Move towards the player
+# attack the player (move into them, then stop for a short time)
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func take_damage(value: int, source: Node2D = self, heavy_strike: bool = false):
+	super(value, source, heavy_strike)
+	soft_animation_player.play("damage")
