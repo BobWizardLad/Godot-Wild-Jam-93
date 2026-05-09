2d1835923ef5c3c1bad68398f379414672ba360c
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-09 13:02:30 -0500

    Melee Damage Area
    Melee area requires an external timer to cooldown between attack
    opportunities

---
+++ godot-jam-93/script/melee_area.gd
@@ -0,0 +1,14 @@
+class_name MeleeArea
+extends BulletArea
+
+@export var attack_cooldown: Timer
+
+## Melee area will make a detection attempt only when damage is off cooldown
+func handle_detected_body_or_area(collided_node: CollisionObject2D):
+	## Collide and disable
+	if collided_node is Unit && attack_cooldown.is_stopped():
+		attacked_body.emit(collided_node) # Notify attack instance
+		collided_node.take_damage(damage, self, is_heavy_strike)
+		attack_cooldown.start()
+	else:
+		return
