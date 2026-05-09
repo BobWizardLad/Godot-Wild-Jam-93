54580c938ff053da4e12d3cd0e7665ed6967be78
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-08 20:31:47 -0500

    basic pathfinding for enemies
    Enemies use pathfindingagent2d for pathing, and a 2d region is in the
    test scene to leverage this.

--- godot-jam-93/script/enemy.gd
+++ godot-jam-93/script/enemy.gd
@@ -4,12 +4,29 @@ extends Unit
 @onready var soft_animation_player: AnimationPlayer = $SoftAnimationPlayer
 @onready var nav_agent: NavigationAgent2D = $NavAgent
 
+@export var nav_target: Node2D
+
 # Move towards the player
 # attack the player (move into them, then stop for a short time)
 
-func _physics_process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	nav_agent.target_position = nav_target.global_position
+
+func _physics_process(_delta: float) -> void:
+	#nav_agent.target_position = get_tree().get_first_node_in_group("Player").global_position
+	if !nav_agent.is_target_reached():
+		var nav_point_direction = to_local(nav_agent.get_next_path_position()).normalized()
+		velocity = nav_point_direction * SPEED
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
 
 func take_damage(value: int, source: Node2D = self, heavy_strike: bool = false):
 	super(value, source, heavy_strike)
 	soft_animation_player.play("damage")
+
+
+func _on_pathfinder_timer_timeout() -> void:
+	if nav_agent.target_position != nav_target.global_position:
+		nav_agent.target_position = nav_target.global_position
+	$PathfinderTimer.start()
