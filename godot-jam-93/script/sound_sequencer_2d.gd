3942959cb58a4f4b3b0716f6cabff9c0899143d9
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-11 10:25:32 -0500

    Sound Sequencer test success
    Object that can queue arbitrary sound effects to layer over each other,
    and allow for a finite # of same sound effects to occour at once

---
+++ godot-jam-93/script/sound_sequencer_2d.gd
@@ -0,0 +1,37 @@
+class_name SoundSequencer2D
+extends Node2D
+
+## Node that queues positional sound effects at its location up to a number of times, allowing
+## layering of the same or different sound effect instances
+
+@export var max_players_per_sound: int
+
+var active_players: Array[AudioStreamPlayer2D]
+
+func _ready() -> void:
+	active_players = []
+
+func _queue_audio_track(track: AudioStream):
+	if get_active_players_count(track) < max_players_per_sound:
+		create_player_and_play(track)
+
+func get_active_players_count(track: AudioStream) -> int:
+	var count = 0
+	for each in active_players:
+		if each.stream == track:
+			count += 1
+	return count
+
+func create_player_and_play(track: AudioStream) -> void:
+	var player = AudioStreamPlayer2D.new()
+	player.stream = track
+	player.finished.connect(cleanup_idle_players)
+	active_players.append(player)
+	add_child(player)
+	player.play()
+
+func cleanup_idle_players() -> void:
+	for each in active_players:
+		if each.playing == false:
+			active_players.erase(each)
+			each.queue_free()
