1814677646882a081cb1c6369f40eb744b0f0453
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-12 11:09:15 -0500

    UpgradeTreeNode Subclasses sketch

---
+++ godot-jam-93/script/upgrade_tree/upgrade_tree_rare.gd
@@ -0,0 +1,9 @@
+class_name UpgradeTreeRare
+extends UpgradeTreeNode
+
+## Special upgrade tree node that has outgoing connections like root AND a synergy
+## that enhences upgrades associated with it (in it or adjacent to it)
+
+@export var synergy : UpgradeRes.UpgradeSynergy
+@export var connections_out: Array[UpgradeTreeNode]
+@export var max_connections: int
---
+++ godot-jam-93/script/upgrade_tree/upgrade_tree_very_rare.gd
@@ -0,0 +1,4 @@
+class_name UpgradeTreeVeryRare
+extends UpgradeTreeNode
+
+## Upgrade Node with only one root node that accesses specific upgrades
