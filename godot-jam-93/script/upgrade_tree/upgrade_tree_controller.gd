b2d6d7f9e346fd3afd131ddd82eed541d18793e6
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-13 10:18:11 -0500

    Applied upgrades

--- godot-jam-93/script/upgrade_tree/upgrade_tree_controller.gd
+++ godot-jam-93/script/upgrade_tree/upgrade_tree_controller.gd
@@ -16,21 +16,30 @@ func get_visual_upgrade_tree() -> Node2D:
 	return Node2D.new()
 
 ## Check to see if a potential
-func is_neighbor_valid(this_neighbor: UpgradeTreeNode, connection_idx: int) -> bool:
-	return this_neighbor.root_node.joint == connections_out[connection_idx].joint
+func is_neighbor_valid(incoming_connection: UpgradeTreeConnection, my_connection: UpgradeTreeConnection) -> bool:
+	return incoming_connection.joint == my_connection.joint
 
-func add_neighbor(this_neighbor: UpgradeTreeNode, connection_idx: int) -> bool:
-	if is_neighbor_valid(this_neighbor, connection_idx):
+func add_neighbor_at(this_neighbor: UpgradeTreeNode, connection_idx: int) -> bool:
+	if is_neighbor_valid(this_neighbor.root_connection, connections_out[connection_idx]):
 		connections_out[connection_idx].neighbor = this_neighbor
 		return true
 	else:
 		return false
 
+func add_new_neighbor(upgrade_node: UpgradeTreeNode) -> bool:
+	for child in connections_out:
+		if is_neighbor_valid(upgrade_node.root_connection, child):
+			child.neighbor = upgrade_node
+			add_child(upgrade_node)
+			apply_upgrades_in_tree(get_parent())
+			return true
+	return false
+
 func neighbor_children_if_connection() -> bool:
 	for child in get_children():
 		if child is UpgradeTreeNode:
 			for connection in connections_out:
-				if child.root_connection.joint == connection.joint:
+				if is_neighbor_valid(child.root_connection, connection):
 					connection.neighbor = child
 					child.root_connection.neighbor = self
 		assert(child.root_connection.neighbor != null, "UpgradeTreeRare " + name + " has an incompatible child " + child.name)
