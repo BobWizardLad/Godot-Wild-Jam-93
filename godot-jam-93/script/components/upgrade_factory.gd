61c40fb2e697bdba6a7153978038522fbe61b9ea
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-15 14:55:01 -0500

    Build Mechanics Tree Test Success

--- godot-jam-93/script/components/upgrade_factory.gd
+++ godot-jam-93/script/components/upgrade_factory.gd
@@ -31,7 +31,19 @@ func get_common() -> UpgradeTreeCommon:
 	return upgrade_node
 
 func get_rare() -> UpgradeTreeRare:
-	return
+	var upgrade_node: UpgradeTreeRare = rare_upgrade_archetypes[randi_range(0, rare_upgrade_archetypes.size()-1)].instantiate()
+	for each in range(0, randi_range(1, rare_max_upgrades)): # randomly 1-MAX upgrades in node
+		var new_upgrade: UpgradeRes = rare_upgrade_pool[randi_range(0, rare_upgrade_pool.size()-1)].duplicate(true) # draw an upgrade from pool
+		new_upgrade.init_effect_value()
+		upgrade_node.upgrades.append(new_upgrade)
+	upgrade_node.position = Vector2(randf_range(spawn_min_X, spawn_max_X), randf_range(spawn_min_Y, spawn_max_Y))
+	return upgrade_node
 
 func get_very_rare() -> UpgradeTreeVeryRare:
-	return
+	var upgrade_node: UpgradeTreeVeryRare = very_rare_upgrade_archetypes[randi_range(0, very_rare_upgrade_archetypes.size()-1)].instantiate()
+	for each in range(0, randi_range(1, very_rare_max_upgrades)): # randomly 1-MAX upgrades in node
+		var new_upgrade: UpgradeRes = very_rare_upgrade_pool[randi_range(0, very_rare_upgrade_pool.size()-1)].duplicate(true) # draw an upgrade from pool
+		new_upgrade.init_effect_value()
+		upgrade_node.upgrades.append(new_upgrade)
+	upgrade_node.position = Vector2(randf_range(spawn_min_X, spawn_max_X), randf_range(spawn_min_Y, spawn_max_Y))
+	return upgrade_node
