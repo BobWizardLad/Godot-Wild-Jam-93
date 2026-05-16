cc1b2efdc5e73ec254891bafc3db81de9bd7f89c
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-16 11:13:41 -0500

    Pickup odds scale based on wave count

--- pickup_spawner.gd
+++ pickup_spawner.gd
@@ -9,7 +9,10 @@ func _ready() -> void:
 
 func spawn_pickup(pickup_position: Vector2) -> void:
 	var roll = randf()
-	if roll <= pickup_chance:
+	if roll <= calculate_pickup_chance(get_tree().get_first_node_in_group("EndlessGameManager").wave_count):
 		var pickup : Area2D = pickups_pool[randi_range(0, pickups_pool.size()-1)].instantiate()
 		pickup.global_position = pickup_position
 		get_tree().current_scene.add_child(pickup)
+
+func calculate_pickup_chance(wave_count: int) -> float:
+	return .666 * (1.5/wave_count)
