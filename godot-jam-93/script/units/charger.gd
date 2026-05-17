bb63dda7c0143b23556b76b828eb45b9d13478af
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-17 00:16:43 -0500

    Charger windup phase for telegraphed atk

--- godot-jam-93/script/units/charger.gd
+++ godot-jam-93/script/units/charger.gd
@@ -12,17 +12,24 @@ var is_charging: bool = false
 ## Function that returns the calculated velocity of a unit.
 ## Navigates to player, then charges at them
 func derive_unit_velocity() -> Vector2:
+	
+	if soft_animation_player.is_playing() && soft_animation_player.current_animation == "windup":
+		return Vector2.ZERO
 	# If already charging, use the charge for velocity
-	if is_charging:
+	elif is_charging:
 		return charge_direction * charge_speed
 	# if close enough to the player, charge at them
 	elif nav_agent.distance_to_target() <= charge_distance:
-		is_charging = true
-		charge_direction = to_local(nav_agent.get_next_path_position()).normalized()
-		charging_timer.start()
+		soft_animation_player.play("windup")
 		return charge_direction * charge_speed
 	else:
 		return super()
 
 func _on_charging_timer_timeout() -> void:
 	is_charging = false
+
+## Begin charging at the player
+func charge() -> void:
+	is_charging = true
+	charge_direction = to_local(nav_agent.get_next_path_position()).normalized()
+	charging_timer.start()
