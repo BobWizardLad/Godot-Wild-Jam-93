2bb1f4598ed03b4babfa82a00af352453b849264
BobWizardLad <bobwizardlad@Gmail.com>
2026-05-17 00:36:13 -0500

    Pickup weighted drop distributions

--- pickup_spawner.gd
+++ pickup_spawner.gd
@@ -1,8 +1,12 @@
 extends Node
-
+## Pool of possible pickups
 @export var pickups_pool : Array[PackedScene]
-## Percentage chance a pickup will be spawned
-@export_range(0.0, 1.0) var pickup_chance: float
+## Chance of each pickup type being spawned
+## MUST be a float between 0.0 and 1.0.
+## Spawns are decided by subtracting the spawn chance from the rolled
+## percentile. If the sum of your spawn chance pool  is > 1.0, then anything after
+## 1.0 as the list is summed will not have a chance of spawning
+@export var spawn_chance: Array[float]
 
 func _ready() -> void:
 	add_to_group("PickupSpawner", true)
@@ -16,3 +20,16 @@ func spawn_pickup(pickup_position: Vector2) -> void:
 
 func calculate_pickup_chance(wave_count: int) -> float:
 	return .666 * (1.5/wave_count)
+
+## Decides what randomized spawn will occour
+## Spawns are decided by subtracting the spawn chance from the rolled
+## percentile. If the sum of your spawn chance pool  is > 1.0, then anything after
+## 1.0 as the list is summed will not have a chance of spawning
+## Spawns the first enemy in the pool if odds do not add up to 1.0
+func get_spawn_from_pool() -> PackedScene:
+	var percentile: float = randf_range(0.0, 1.0)
+	for each in range(0, pickups_pool.size()):
+		percentile -= spawn_chance[each]
+		if percentile <= 0.0:
+			return pickups_pool[each]
+	return pickups_pool[0] # Return the first option if we do not reach a conclusion
